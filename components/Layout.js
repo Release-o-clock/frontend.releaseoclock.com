0fc29feda3a62bdf305173d111ea888001df41a6
Iulian Beleiu <iulian.beleiu@gmail.com>
2022-02-05 10:49:10 +0200

    add coming soon

--- components/Layout.js
+++ components/Layout.js
@@ -4,8 +4,8 @@ import Navigation from "./Navigation"
 const Layout = ({children, global}) => {
     return (
         <>
-        <Navigation global={global}/>
-            <main>
+        {/* <Navigation global={global}/> */}
+            <main global={global}>
                 {children}
             </main>
         </>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,29 @@
-import HeroTeaser from "../components/pages/home/HeroTeaser"
-export default function Home() {
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+const Home = ({ global }) => {
   return (
-      <HeroTeaser />
+    <div className={styles.container}>
+      <Head>
+        <title>{global.data.attributes.siteName}</title>
+        <meta name="description" content="Release o'clock website" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          {global.data.attributes.siteName}
+        </h1>
+
+        <div className={styles.grid}>
+          
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <p>Powered by Release o'clock</p>
+      </footer>
+    </div>
   )
 }
+
+export default Home
